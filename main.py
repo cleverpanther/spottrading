bf2dcddb34949e386a19ac17ba87b7c12ae4c35b
titanrtx0714 <titanrtx0714@gmail.com>
2024-03-28 14:31:05 -0700

    updated

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-import smtplib
-import time
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 import requests
 import time
 import hashlib
 import hmac
-import uuid
 import resend
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 import json
+from dotenv import load_dotenv
+import os
+from datetime import datetime, timedelta
+import re
+import sys
 
+load_dotenv()
+
+url=os.getenv('url')
+api_key=os.getenv('api_key')
+secret_key = os.getenv('secret_key')
+symbol = os.getenv('symbol')
+from_email = os.getenv('from')
+to_email = os.getenv('to')
+resend_api_key = os.getenv('resend_api_key')
+subject = "Bybit Auto-Bot"
+threshold = int(os.getenv('threshold'))
+
+print("Initialized Server")
+
+expdates = []
 httpClient=requests.Session()
 recv_window=str(5000)
-# url="https://api-testnet.bybit.com" # Testnet endpoint
-url="https://api.bybit.com"
+
 def HTTP_Request(endPoint,method,payload,Info):
     global time_stamp
     time_stamp=str(int(time.time() * 10 ** 3))
@@ -33,7 +44,6 @@ def HTTP_Request(endPoint,method,payload,Info):
         response = httpClient.request(method, url+endPoint, headers=headers, data=payload)
     else:
         response = httpClient.request(method, url+endPoint+"?"+payload, headers=headers)
-    print(Info + " Elapsed Time : " + str(response.elapsed))
     return response.text
 
 def genSignature(payload):
@@ -42,78 +52,128 @@ def genSignature(payload):
     signature = hash.hexdigest()
     return signature
 
-def gatherdata():
+def parse_symbol(symbol):
+    match = re.search(r'(\d+)(JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)(\d+)-(\d+)', symbol)
+    if match:
+        day, month, year, strike_price = match.groups()
+        date = datetime.strptime(f"{day} {month} {year}", "%d %b %y")
+        return date, int(strike_price)
+    else:
+        return None
+
+def firststep(now):
     endpoint="/v5/market/tickers"
     method="GET"
-    params1='category=option&symbol=BTC-29MAR24-48000-C&side=Sell'
+    params='category=option&baseCoin=BTC'
+    text = HTTP_Request(endpoint,method,params,"UnFilled")
+    json_data = json.loads(text)
+    option_list = json_data['result']['list']
+    sorted_list = sorted(option_list, key=lambda x: parse_symbol(x['symbol']))
+    alldate = sorted(set([parse_symbol(item['symbol'])[0] for item in sorted_list]))
+    upperdate = now + timedelta(days=20)
+    canddate = []
+    for date in alldate:
+        if date >= now and date < upperdate:
+            canddate.append(date)
+        else:
+            continue
+    return canddate
+
+def secondstep(expdate):
+    endpoint="/v5/market/tickers"
+    method="GET"
+    params='category=option&baseCoin=BTC&expDate=' + expdate
+    text = HTTP_Request(endpoint,method,params,"UnFilled")
+    # print(text)
+    json_data = json.loads(text)
+    option_list = json_data['result']['list']
+    sorted_list = sorted(option_list, key=lambda x: parse_symbol(x['symbol']))
+    strikeprice = sorted(set([parse_symbol(item['symbol'])[1] for item in sorted_list]))
+    candprice = []
+    num = 0
+    for price in strikeprice:
+        candprice.append(price)
+        num = num + 1
+        if num == 4:
+            break
+    return candprice
+
+def thirdstep(expdate, strike_price):
+    strike_price = str(strike_price)
+    endpoint="/v5/market/tickers"
+    method="GET"
+    params1='category=option&symbol=' + symbol + '-' + expdate + '-' + strike_price + '-C&side=Sell'
     text1 = HTTP_Request(endpoint,method,params1,"UnFilled")
     data1 = json.loads(text1)
     Call_BidPrice = float(data1['result']['list'][0]['bid1Price'])
 
     endpoint="/v5/market/tickers"
     method="GET"
-    params1='category=option&symbol=BTC-29MAR24-48000-P&side=Buy'
+    params1='category=option&symbol=' +  symbol + '-' + expdate + '-' + strike_price + '-P&side=Buy'
     text2 = HTTP_Request(endpoint,method,params1,"UnFilled")
     data2 = json.loads(text2)
     Put_AskPrice = float(data2['result']['list'][0]['ask1Price'])
 
     endpoint="/v5/market/tickers"
     method="GET"
-    params1='category=linear&symbol=BTCUSDT'
+    params1='category=linear&symbol=' + symbol + 'USDT'
     text3 = HTTP_Request(endpoint,method,params1,"UnFilled")
     data3 = json.loads(text3)
     Perp_AskPrice = float(data3['result']['list'][0]['ask1Price'])
     
-    Profitability = (Call_BidPrice - Put_AskPrice) - (Perp_AskPrice - 48000)
-    
-    return Profitability
+    Profitability = (Call_BidPrice - Put_AskPrice) - (Perp_AskPrice - int(strike_price))
+    result = expdate + '-' + strike_price + '=>' + str(Profitability)
 
-# # Email configuration
-# sender_email = "titanrtx0714@outlook.com"
-# # sender_password = "infinite0714"
-# receiver_email = "titanrtx1010102@hotmail.com"
-subject = "Bybit Data"
-api_key='3GsEVrhzzOnMusaoSt'
-secret_key ='y90nFfUgTEUFoDsXHpkduxw4cxrOQjvbfApI'
+    return [result, Profitability]
 
 # Create a function to send email
-def send_email(subject, body):
-    resend.api_key = "re_enoinqaw_Dhxn8mSSK3wkD25SfaZV2jhK"
+def send_email(subject, result, body):
+    resend.api_key = resend_api_key
+    r = resend.Emails.send({
+        "from": from_email,
+        "to": to_email,
+        "subject": subject,
+        "html": "Positive P&L: " + result
+        })
+    print('Positive P$L: ' + str(result))
+    print("Email Already Sent.")
 
-    if body > 500:
-        r = resend.Emails.send({
-            "from": "onboarding@resend.dev",
-            "to": "titanrtx0714@gmail.com",
-            "subject": subject,
-            "html": "Good Situation: " + str(body)
-            })
-    else:
-        r = resend.Emails.send({
-            "from": "onboarding@resend.dev",
-            "to": "titanrtx0714@gmail.com",
-            "subject": subject,
-            "html": "Bad Situation: " + str(body)
-            })
-
-    # # Create a multipart message
-    # message = MIMEMultipart()
-    # message["From"] = sender_email
-    # message["To"] = receiver_email
-    # message["Subject"] = subject
-
-    # # Add body to email
-    # message.attach(MIMEText(body, "plain"))
-
-    # # Connect to SMTP server
-    # with smtplib.SMTP("smtp-mail.outlook.com", port=587) as server:
-    #     server.starttls()  # Secure the connection
-    #     server.login(sender_email, sender_password)
-    #     server.sendmail(sender_email, receiver_email, message.as_string())
+def progress_bar(current, total):
+    steps = 50
+    progress = int(current / total * steps)
+    percentage = float(progress * 100 / steps)
+    bar_str = '[' + '=' * progress + ' ' * (steps - progress) + ']'
+    sys.stdout.write(f'\r{bar_str} {percentage}% completed')
+    sys.stdout.flush()
 
 # Loop to send email every 1 minute
 while True:
-    body = gatherdata()
-    print(body)
-    send_email(subject=subject, body=body)
-    print("Email sent successfully.")
-    time.sleep(10)  # Sleep for 60 seconds (1 minute)
+    now = datetime.now()
+    current_output = 'Now is ' + str(now) + '.'
+    print(current_output)
+    canddate = firststep(now)
+    datenum = canddate.__len__()
+    outputbody = ""
+    total_iterations = len(canddate) * 4
+    current_iteration = 0
+    profitability_this_period = []
+    for date in canddate:
+        expdate = date.strftime("%d%b%y").upper().lstrip("0")
+        candstrikeprice = secondstep(expdate=expdate)
+        for strikeprice in candstrikeprice:
+            [result, profitability] = thirdstep(expdate, strikeprice)
+            profitability_this_period.append(profitability)
+            current_iteration += 1
+            progress_bar(current_iteration, total_iterations)
+            if profitability > threshold:
+                outputbody = outputbody + result + '\n'
+            time.sleep(1)
+    progress_bar(total_iterations, total_iterations)
+    print()
+    if outputbody == "":
+        print("There is no appropriate ticker.")
+    else:
+        send_email(subject=subject, result = result, body=outputbody)
+        print("Email Already Sent!")
+    print(profitability_this_period, "Process Finished.")
+    time.sleep(5)  # Sleep for 60 seconds (1 minute)
